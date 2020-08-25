1d9761f760435904059ae0ea0bec8a45176b42e9
chelsejw <chelsejw@gmail.com>
2020-08-25 10:50:16 +0800

    Attempt #1 to define singly linked list class

--- data-structures/singly-linked-list/singly-linked-list.js
+++ data-structures/singly-linked-list/singly-linked-list.js
@@ -7,3 +7,28 @@ class Node {
         this.next = null;
     }
 }
+
+class SinglyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(value) {
+        const newNode = new Node(value)
+
+        //If the list has no head yet -- it's empty. Set the new node to be the tail and head.
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode
+        
+        //Else, the list is not empty. Assign new node to the current tail first, then change the tail to new node. 
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++;
+    }
+}
+

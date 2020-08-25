bc61b5e3b648c035557e3453f5fb4e6844245956
chelsejw <chelsejw@gmail.com>
2020-08-25 18:29:51 +0800

    Add comments for shift

--- data-structures/doubly-linked-list/doubly-linked-list.js
+++ data-structures/doubly-linked-list/doubly-linked-list.js
@@ -68,6 +68,21 @@ class DoublyLinkedList {
         this.length--;
         return oldHead
     }
+
+    unshift(value) {
+        const newNode = new Node(value);
+
+        if (this.length===0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        length++;
+        return this;
+    } 
     
     get(index){
         if (index < 0 || index > this.length - 1) return false;
@@ -98,6 +113,6 @@ const createDoubleLinkedList = (arr) => {
 }
 
 let list = createDoubleLinkedList([1,2])
-console.log(list.shift())
+console.log(list.unshift(0))
 list.print()
 console.log(list)

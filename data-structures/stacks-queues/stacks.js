fa2bdaf62c0fb4c134da33cfb28b5fdda08b00c2
chelsejw <chelsejw@gmail.com>
2020-08-27 00:37:34 +0800

    Add stacks implementation - push and pop

---
+++ data-structures/stacks-queues/stacks.js
@@ -0,0 +1,59 @@
+//PRINCIPLE: Last in, first out;
+
+const stack1 = [];
+stack1.push("Item 1")
+stack1.push("Item 2")
+stack1.pop();
+
+//Push and pop are more efficient than shift/unshift because inserting/deleting at the end of the array would not require re-indexing everything
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0
+    }
+
+    push(value) {
+        if (!value) return false;
+        let newNode = new Node(value)
+        if (!this.first) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            let temp = this.first;
+            newNode.next = temp;
+            this.first = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        const temp = this.first
+        if (this.length === 1) {
+            this.last = null;
+            this.first = null;
+        } else {
+            this.first = temp.next;
+        }
+        this.length--;
+        return temp;
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+const node1 = new Node("hello")
+const stack = new Stack();
+console.log(stack.push("hello"));
+console.log(stack.push("there"));
+console.log(stack.push("world"));
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack)

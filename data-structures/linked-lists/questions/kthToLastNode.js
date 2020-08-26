e3104c098487143465085990b7d396edd60cd4b3
chelsejw <chelsejw@gmail.com>
2020-08-26 12:37:53 +0800

    General test cases work, not sure about expected value to return for invalid k cases though, cannot pass test on IC

--- data-structures/linked-lists/questions/kthToLastNode.js
+++ data-structures/linked-lists/questions/kthToLastNode.js
@@ -1,26 +1,30 @@
 const node = require('./classes.js').module
 
-let nodes = node.valuesToLinkedListNodes([1, 2, 3, 4, 5, 6]);
+let nodes = node.valuesToLinkedListNodes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+
+// NODE THAT WILL BE RETURNED according to k
+// nodes: [ 1 , 2 , 3 , 4 , 5 , 6 ]
+// k:     [ 6,  5,  4,  3,  2,  1 ]
 
 function kthToLastNode(k, head) {
   // Return the kth to last node in the linked list
-  if (k < 1) return undefined;
+  if (k < 1) return null;
   let current = head;
   let length = 0;
 
   //Traverse through list once to get length
   while (current) {
     length++;
-      if (!current.next.next && k === 1) return current;
+    if (k===1 && !current.next) return current;
     current = current.next;
   }
-  if (k > length) return undefined;
+  if (k > length) return null;
   if (k===length) return head;
 
   let position = length - k;
   let index = 0;
   current = head;
-
   // Traverse once more
   while (index < position) {
     index++;

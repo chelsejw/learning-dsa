3ff977646891d141fb03bc6464f55e4d1bfcbbd9
chelsejw <chelsejw@gmail.com>
2020-09-11 18:54:43 +0800

    start binary search trees implementation

---
+++ data-structures/binary-search-trees/methods.js
@@ -0,0 +1,24 @@
+class Node {
+    constructor(val){
+        this.val = val
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+
+    insert(node) {
+        if (!this.root) {
+            return this.root = node;
+        }
+
+
+
+    }
+
+
+}

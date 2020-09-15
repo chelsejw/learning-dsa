e56225d947a1c0a70a4e2cf64940235c019aeebd
chelsejw <chelsejw@gmail.com>
2020-09-16 01:08:53 +0800

    attempt at extractmax for binary heap

--- data-structures/binary-heaps/methods.js
+++ data-structures/binary-heaps/methods.js
@@ -18,6 +18,48 @@ class MaxBinaryHeap {
         console.log(this.values)
         return this.values
     }
+
+    removeRoot() {
+        const max = this.values[0]; //Previous max
+        const end = this.values.pop();
+        this.values[0] = end;
+        const swap = (idx1, idx2) => {
+            const temp = this.values[idx1];
+            this.values[idx1] = this.values[idx2];
+            this.values[idx2] = temp;
+            return;
+        }
+
+        let parentIndex = 0;
+
+        while (parentIndex < this.values.length-1) {
+            let newRoot = this.values[parentIndex];
+            let leftChildIdx = parentIndex*2+1
+            let rightChildIdx = parentIndex*2+2
+            let leftChild = this.values[leftChildIdx];
+            let rightChild = this.values[rightChildIdx];
+            console.log(newRoot)
+            if (leftChild > newRoot && rightChild > newRoot) {
+              if (leftChild > rightChild) {
+                swap(leftChildIdx, parentIndex);
+                parentIndex = leftChildIdx;
+              } else {
+                swap(rightChildIdx, parentIndex);
+                parentIndex = rightChildIdx;
+              }
+            } else if (leftChild > newRoot) {
+              swap(leftChildIdx, parentIndex);
+              parentIndex = leftChildIdx;
+            } else if (rightChild > newRoot) {
+              swap(rightChildIdx, parentIndex);
+              parentIndex = rightChildIdx;
+            } else {
+              break;
+            };            
+        };
+        console.log(this.values);
+        return max;
+    }
 }
 
 
@@ -29,3 +71,4 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
+console.log(heap.removeRoot())

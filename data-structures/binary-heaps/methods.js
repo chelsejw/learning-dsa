76d1414dd0b7716fe479114bb6dd195e0f69827d
chelsejw <chelsejw@gmail.com>
2020-09-16 11:44:06 +0800

    implement extact max

--- data-structures/binary-heaps/methods.js
+++ data-structures/binary-heaps/methods.js
@@ -22,7 +22,12 @@ class MaxBinaryHeap {
     removeRoot() {
         const max = this.values[0]; //Previous max
         const end = this.values.pop();
-        this.values[0] = end;
+        if (this.values.length > 0) this.values[0] = end;
+        this.sinkDown();
+        return max;
+    }
+
+    sinkDown() {
         const swap = (idx1, idx2) => {
             const temp = this.values[idx1];
             this.values[idx1] = this.values[idx2];
@@ -30,35 +35,31 @@ class MaxBinaryHeap {
             return;
         }
 
-        let parentIndex = 0;
+        let parentIdx = 0;
+        const length = this.values.length;
+        let element = this.values[parentIdx];
 
-        while (parentIndex < this.values.length-1) {
-            let newRoot = this.values[parentIndex];
-            let leftChildIdx = parentIndex*2+1
-            let rightChildIdx = parentIndex*2+2
-            let leftChild = this.values[leftChildIdx];
-            let rightChild = this.values[rightChildIdx];
-            console.log(newRoot)
-            if (leftChild > newRoot && rightChild > newRoot) {
-              if (leftChild > rightChild) {
-                swap(leftChildIdx, parentIndex);
-                parentIndex = leftChildIdx;
-              } else {
-                swap(rightChildIdx, parentIndex);
-                parentIndex = rightChildIdx;
-              }
-            } else if (leftChild > newRoot) {
-              swap(leftChildIdx, parentIndex);
-              parentIndex = leftChildIdx;
-            } else if (rightChild > newRoot) {
-              swap(rightChildIdx, parentIndex);
-              parentIndex = rightChildIdx;
-            } else {
-              break;
-            };            
-        };
-        console.log(this.values);
-        return max;
+        while (true) {
+            let swapIdx = null;
+            let leftIdx = parentIdx * 2 + 1;
+            let rightIdx = parentIdx * 2 + 2;
+            let leftChild, rightChild;
+            if (leftIdx < length) {
+                leftChild = this.values[leftIdx];
+                if (leftChild > element) swapIdx = leftIdx;
+            }
+            if (rightIdx < length) {
+                rightChild = this.values[rightIdx];
+                if (!swap && rightChild > element || swap && rightChild > leftChild) {
+                  swapIdx = rightIdx
+                }
+            }
+            if (!swapIdx) break;
+            swap(swapIdx, parentIdx);
+            parentIdx = swapIdx;
+        }
+        return console.log(this.values);
+
     }
 }
 

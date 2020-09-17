9b1eaf67b99edeb1bbe848925409bd7e1e889929
chelsejw <chelsejw@gmail.com>
2020-09-17 14:02:16 +0800

    implement adjacency list addVertex, add/remove edges

---
+++ data-structures/graphs/methods.js
@@ -0,0 +1,34 @@
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex){
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+        return this.adjacencyList;
+    }
+
+    removeEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(item => item!==vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(item => item!==vertex1);
+        return this.adjacencyList;
+    }
+}
+
+const graph = new Graph();
+graph.addVertex("Tokyo");
+graph.addVertex("Berlin");
+graph.addVertex("Moscow");
+console.log(graph.adjacencyList);
+
+graph.addEdge("Berlin", "Moscow");
+graph.addEdge("Berlin", "Tokyo");
+console.log(graph.adjacencyList);
+
+graph.removeEdge("Berlin", "Moscow");
+console.log(graph.adjacencyList);

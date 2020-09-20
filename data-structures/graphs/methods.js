d1780a8beebb5371aeb79f3c7ea72bb5307339ce
chelsejw <chelsejw@gmail.com>
2020-09-20 14:19:26 +0800

    add dfs for graphs

--- data-structures/graphs/methods.js
+++ data-structures/graphs/methods.js
@@ -35,17 +35,47 @@ class Graph{
         delete this.adjacencyList[vertex];
     }
 
+    DFS(start) {
+        const vertices = []; //[A, B, D, ]
+        const visited = {}; // {A: true, B: true, D: true}
+
+        const helper = (vertex) => {
+            const adjacentNodes = this.adjacencyList[vertex]
+            if (visited[vertex]) return;
+
+            vertices.push(vertex);
+            visited[vertex] = true;
+
+            if (adjacentNodes.length < 1) return;
+
+            for (let i = 0; i < adjacentNodes.length; i++) {
+                let neighbour = adjacentNodes[i]
+                if (!visited[neighbour]) helper(neighbour);
+            }
+        }
+        helper(start);
+        return vertices;
+
+    }
+
 }
 
 const graph = new Graph();
-graph.addVertex("Tokyo");
-graph.addVertex("Berlin");
-graph.addVertex("Moscow");
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+
 console.log(graph.adjacencyList);
 
-graph.addEdge("Berlin", "Moscow");
-graph.addEdge("Berlin", "Tokyo");
-console.log(graph.adjacencyList);
-
-graph.removeVertex("Moscow");
-console.log(graph.adjacencyList);
+console.log(graph.DFS("A"));
